7ad9814fed168151c67a3ba7bfa580d9eb689261
ukstv <sergey@ukstv.me>
2020-12-25 14:40:21 +0400

    permit

--- maskodid/backbone/backbone.tsx
+++ maskodid/backbone/backbone.tsx
@@ -6,7 +6,6 @@ import {
 } from "@silentcastle/keys";
 import { BehaviorSubject } from "rxjs";
 import { Subjectable } from "./use-subjectable";
-import { PermissionsAuthenticateScreen } from "../screens/permissions/permissions-authenticate.screen";
 
 const privateKeyFactory = new PrivateKeyFactory();
 
@@ -72,35 +71,6 @@ export class Backbone {
     const publicKey = await privateKey.publicKey();
     const f = fingerprint(publicKey);
     return `did:key:${f}`;
-  }
-
-  async askPermissionAuthenticate(origin: string) {
-    return new Promise<void>((resolve, reject) => {
-      const handleDecision = (isPermitted: boolean) => {
-        if (isPermitted) {
-          resolve()
-        } else {
-          reject(new Error('denied'))
-        }
-        this.goHome()
-      };
-      const page = (
-          <PermissionsAuthenticateScreen
-              origin={origin}
-              onDecision={handleDecision}
-          />
-      );
-    })
-  }
-
-  async authenticate(origin: string) {
-    await this.askPermissionAuthenticate(origin);
-    return new Date().toISOString();
-    // const seed = localStorage.getItem("maskodid:seed");
-    // const privateKey = privateKeyFactory.fromSeed(AlgorithmKind.ed25519, seed);
-    // const publicKey = await privateKey.publicKey();
-    // const f = fingerprint(publicKey);
-    // return `did:key:${f}`;
   }
 }
 
